0e343eb5900d067f4ae466866504fc70e928c8bb
Famouse <famouse_ru@mail.ru>
2022-05-15 14:04:33 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Artificial_Intelligence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

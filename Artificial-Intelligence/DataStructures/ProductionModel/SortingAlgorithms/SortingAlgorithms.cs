84c7359dbb2387d6c12dc239c07574cba4aaf052
Famouse <famouse_ru@mail.ru>
2022-05-20 22:07:29 +0300

    Changing rule architecture

--- Artificial-Intelligence/DataStructures/ProductionModel/SortingAlgorithms/SortingAlgorithms.cs
+++ Artificial-Intelligence/DataStructures/ProductionModel/SortingAlgorithms/SortingAlgorithms.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,110 +10,155 @@ namespace Artificial_Intelligence.DataStructures.ProductionModel.SortingAlgorith
 {
     public static class SortingAlgorithms
     {
-        private static bool RequestCondition(out string nameof, params object[] @params)
+        public static int ReadInt()
         {
-            // 2+ null is bad
-            // 1 null is good
-        }
-        private static List<Act<Knowledge>> GetActs()
-        {
-            return new List<Act<Knowledge>>()
+            bool isInt = false;
+            int parseInt = 0;
+
+            while (!isInt)
             {
-                (k, request) => // k - knowledge
+                string key = Console.ReadKey().KeyChar.ToString();
+                isInt = Int32.TryParse(key, out parseInt);
+            }
+
+            return parseInt;
+        }
+
+        public static int ReadByRange(int min, int max)
+        {
+            while (true)
+            {
+                int num = ReadInt();
+                if (num >= min && num <= max)
                 {
-                    if (k.DataSize == null && k.ExecutionSpeed != null)
+                    return num;
+                }
+            }
+        }
+
+        public static string RequestProperty(Knowledge knowledge, PropertyInfo propertyInfo)
+        {
+            int i = 0;
+            string[] enumNames = null;
+            switch (propertyInfo.Name)
+            {
+                case nameof(Knowledge.DataSize):
+                    Console.WriteLine($"Please enter: {nameof(Knowledge.DataSize)}");
+                    enumNames = Enum.GetNames(typeof(DataSize));
+                    enumNames.ToList().ForEach(name => Console.WriteLine($"{i++}. {name}"));
+                    i = ReadByRange(0, enumNames.Length);
+                    propertyInfo.SetValue(knowledge, (DataSize)i);
+                    return enumNames[i];
+
+                case nameof(Knowledge.SortingType):
+                    Console.WriteLine($"Please enter: {nameof(Knowledge.SortingType)}");
+                    enumNames = Enum.GetNames(typeof(SortingType));
+                    enumNames.ToList().ForEach(name => Console.WriteLine($"{i++}. {name}"));
+                    i = ReadByRange(0, enumNames.Length);
+                    propertyInfo.SetValue(knowledge, (SortingType)i);
+                    return enumNames[i];
+
+                case nameof(Knowledge.MemoryUsed):
+                    Console.WriteLine($"Please enter: {nameof(Knowledge.MemoryUsed)}");
+                    enumNames = Enum.GetNames(typeof(MemoryUsed));
+                    enumNames.ToList().ForEach(name => Console.WriteLine($"{i++}. {name}"));
+                    i = ReadByRange(0, enumNames.Length);
+                    propertyInfo.SetValue(knowledge, (MemoryUsed)i);
+                    return enumNames[i];
+
+                case nameof(Knowledge.NecessaryTime):
+                    Console.WriteLine($"Please enter: {nameof(Knowledge.NecessaryTime)}");
+                    enumNames = Enum.GetNames(typeof(NecessaryTime));
+                    enumNames.ToList().ForEach(name => Console.WriteLine($"{i++}. {name}"));
+                    i = ReadByRange(0, enumNames.Length);
+                    propertyInfo.SetValue(knowledge, (NecessaryTime)i);
+                    return enumNames[i];
+
+                case nameof(Knowledge.Stability):
+                    Console.WriteLine($"Please enter: {nameof(Knowledge.Stability)}");
+                    enumNames = new string[] {"No", "Yes"};
+                    enumNames.ToList().ForEach(name => Console.WriteLine($"{i++}. {name}"));
+                    i = ReadByRange(0, enumNames.Length);
+                    propertyInfo.SetValue(knowledge, Convert.ToBoolean(i));
+                    return Convert.ToBoolean(i).ToString();
+
+                case nameof(Knowledge.ExecutionSpeed):
+                    Console.WriteLine($"Please enter: {nameof(Knowledge.ExecutionSpeed)}");
+                    enumNames = Enum.GetNames(typeof(ExecutionSpeed));
+                    enumNames.ToList().ForEach(name => Console.WriteLine($"{i++}. {name}"));
+                    i = ReadByRange(0, enumNames.Length);
+                    propertyInfo.SetValue(knowledge, (ExecutionSpeed)i);
+                    return enumNames[i];
+
+                case nameof(Knowledge.DuplicateData):
+                    Console.WriteLine($"Please enter: {nameof(Knowledge.DuplicateData)}");
+                    enumNames = Enum.GetNames(typeof(DuplicateData));
+                    enumNames.ToList().ForEach(name => Console.WriteLine($"{i++}. {name}"));
+                    i = ReadByRange(0, enumNames.Length);
+                    propertyInfo.SetValue(knowledge, (DuplicateData)i);
+                    return enumNames[i];
+
+                case nameof(Knowledge.Result):
+                    Console.WriteLine($"Please enter: {nameof(Knowledge.Result)}");
+                    enumNames = Enum.GetNames(typeof(Sorting));
+                    enumNames.ToList().ForEach(name => Console.WriteLine($"{i++}. {name}"));
+                    i = ReadByRange(0, enumNames.Length);
+                    propertyInfo.SetValue(knowledge, (Sorting)i);
+                    return enumNames[i];
+                default:
+                    throw new NotSupportedException("Not supported type");
+            }
+        }
+
+        private static List<Rule<Knowledge>> GetRules()
+        {
+            return new List<Rule<Knowledge>>()
+            {
+                new Rule<Knowledge>()
+                {
+                    Input = new string[] {nameof(Knowledge.DataSize), nameof(Knowledge.ExecutionSpeed)},
+                    Returns = nameof(Knowledge.NecessaryTime),
+                    Act = (knowledge, request) =>
                     {
-                        request(nameof(k.DataSize));
-                    }
-                    else if (k.DataSize != null && k.ExecutionSpeed == null)
+
+                    },
+                },
+                new Rule<Knowledge>()
+                {
+                    Input = new string[] {nameof(Knowledge.ExecutionSpeed), nameof(Knowledge.DuplicateData), nameof(Knowledge.Stability)},
+                    Returns = nameof(Knowledge.Result),
+                    Act = (knowledge, request) =>
                     {
-                        request(nameof(k.ExecutionSpeed));
-                    }
+                        Fact speedFact = new Fact(nameof(knowledge.ExecutionSpeed), knowledge.ExecutionSpeed, ExecutionSpeed.Medium);
+                        Fact duplicateDataFact = new Fact(nameof(knowledge.DuplicateData), knowledge.DuplicateData, DuplicateData.Many);
+                        Fact stabilityFact = new Fact(nameof(knowledge.Stability), knowledge.Stability, false);
 
-                    if (k.DataSize == DataSize.Large &&
-                        k.ExecutionSpeed == ExecutionSpeed.Slow)
-                    {
-                        k.NecessaryTime = NecessaryTime.Many;
-                    }
-                },
-                (k, request) =>
-                {
-                    if (k.ExecutionSpeed == null && k.DuplicateData != null && k.Stability != null)
-                    {
-                        request(nameof(k.ExecutionSpeed));
-                    }
-                    else if (k.ExecutionSpeed != null && k.DuplicateData == null && k.Stability != null)
-                    {
-                        request(nameof(k.DuplicateData));
-                    }
-                    else if (k.ExecutionSpeed != null && k.DuplicateData != null && k.Stability == null)
-                    {
-                        request(nameof(k.Stability));
-                    }
-
-                    if (k.ExecutionSpeed == ExecutionSpeed.Medium &&
-                        k.DuplicateData == DuplicateData.Many &&
-                        k.Stability ==  false)
-                    {
-                        k.Result = Sorting.Quicksort;
-                    }
-                },
-                (k, request) =>
-                {
-                    if (k.SortingType == null && k.MemoryUsed != null)
-                    {
-                        request(nameof(k.SortingType));
-                    }
-                    else if (k.SortingType != null && k.MemoryUsed == null)
-                    {
-                        request(nameof(k.MemoryUsed));
-                    }
-
-                    if (k.DataSize == DataSize.Large &&
-                        k.ExecutionSpeed == ExecutionSpeed.Slow)
-                    {
-                        k.NecessaryTime = NecessaryTime.Many;
-                    }
-                },
-                (k, request) =>
-                {
-
-                },
-                (k, request) =>
-                {
-
-                },
-                (k, request) =>
-                {
-
-                },
-                (k, request) =>
-                {
-
-                },
-                (k, request) =>
-                {
-
-                },
-                (k, request) =>
-                {
-
-                },
-                (k, request) =>
-                {
-
-                },
-                (k, request) =>
-                {
-
-                },
-
+                        if (Rule<Knowledge>.CheckFacts(knowledge, request, speedFact, duplicateDataFact, stabilityFact))
+                        {
+                            knowledge.Result = Sorting.Quicksort;
+                        };
+                    },
+                }
             };
         }
 
         public static void Test()
         {
 
+
+            //var a = GetRules();
+
+            //Knowledge knowledge = new Knowledge();
+            //var s = knowledge.DataSize;
+            //{
+            //    ExecutionSpeed = ExecutionSpeed.Medium,
+            //    DuplicateData = null,
+            //    Stability = false
+            //};
+
+            //a[1].Act(knowledge, null);
+
+            //Console.WriteLine(RequestProperty(knowledge, typeof(Knowledge).GetProperty(nameof(knowledge.ExecutionSpeed))));
         }
     }
 }
--- ArtificialIntelligence.DataStructures/ProductionModel/Rule.cs
+++ ArtificialIntelligence.DataStructures/ProductionModel/Rule.cs
@@ -7,20 +7,62 @@ using System.Threading.Tasks;
 
 namespace ArtificialIntelligence.DataStructures.ProductionModel
 {
-    public delegate void RequestPropertyByName(string nameofProperty);
-    public delegate void Act<T>(T knowledge, RequestPropertyByName requestPropertyByName);
+    public delegate string RequestProperty<T>(T knowledge, PropertyInfo propertyInfo);
+    public delegate void Act<T>(T knowledge, RequestProperty<T> request);
     public sealed class Rule<T>
     {
-        private T Knowledge { get; set; }
-        private RequestPropertyByName RequestPropertyByName { get; set; }
-        private Act<T> Act { get; set; }
-        public Rule(T knowledge, RequestPropertyByName requestPropertyByName, Act<T> act)
+        public bool IsSimple => Input.Length == 1;
+
+        public Act<T> Act { get; init; }
+        public String[] Input { get; init; }
+        public String Returns { get; init; }
+        public Rule(Act<T> act, string returns, params string[] input)
         {
-            Knowledge = knowledge;
-            RequestPropertyByName = requestPropertyByName;
-            Act = act;
+            Act = act ?? throw new NullReferenceException(nameof(act));
+            Returns = returns ?? throw new NullReferenceException(nameof(returns));
+            Input = input ?? throw new NullReferenceException(nameof(input)); ;
         }
 
-        public void Check() => Act(Knowledge, RequestPropertyByName);
+        public Rule() { }
+
+        public static bool CheckFacts(T knowledge, RequestProperty<T> request, params Fact[] facts)
+        {
+            int trueFactsNumber = 0; // The number of true facts
+            Fact nullFact = null;
+
+            foreach (var fact in facts)
+            {
+                if (fact.CurrentValue is null)
+                {
+                    nullFact = fact;
+                }
+                else if (fact.CurrentValue.Equals(fact.TrueValue))
+                {
+                    trueFactsNumber++;
+                }
+            }
+
+            if (nullFact is not null && trueFactsNumber == facts.Length - 1)
+            {
+                nullFact.CurrentValue = request(knowledge, typeof(T).GetProperty(nullFact.PropertyName));
+                trueFactsNumber += nullFact.CurrentValue == nullFact.TrueValue ? 1 : 0;
+            }
+
+            return trueFactsNumber == facts.Length ? true : false;
+        }
+    }
+
+    public sealed class Fact
+    {
+        public string PropertyName { get; set; }
+        public object CurrentValue { get; set; }
+        public object TrueValue { get; set; }
+
+        public Fact(string propertyName, object currentValue, object trueValue)
+        {
+            PropertyName = propertyName;
+            CurrentValue = currentValue;
+            TrueValue = trueValue;
+        }
     }
 }
